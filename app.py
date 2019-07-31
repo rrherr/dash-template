522f65fa1dbaf14bc1970436ec3640326068d9f9
Ryan Herr <ryanherr@Ryans-MBP.zyxel.com>
2019-07-31 09:50:12 -0500

    Make mobile-friendly with viewport meta tag

--- app.py
+++ app.py
@@ -48,7 +48,11 @@ external_stylesheets = [
     'https://use.fontawesome.com/releases/v5.9.0/css/all.css', # for social media icons
 ]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+meta_tags=[
+    {'name': 'viewport', 'content': 'width=device-width, initial-scale=1'}
+]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 app.config.suppress_callback_exceptions = True
 app.title = 'YOUR APP NAME' # appears in browser title bar
 server = app.server
